49fe36aef850b8635429fac551602001f1c1d604
Jagatpreet <jnir@crabi-robotics.com>
2025-03-06 17:24:53 -0500

    added time for server lifetime

--- simple_server.py
+++ simple_server.py
@@ -6,17 +6,20 @@ PORT = 9090
 
 # TCP/IP server with sock stream for internet based devices.
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# to allow address reuse if closed unexpectedely.
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server.bind((HOST, PORT))
 
 # start rejecting connections after 5 clients
 server.listen(5)
 
 start = time.time()
+SERVER_LIFETIME = 100
 communication_socket = None
 while True:
     elapsed_time = time.time() - start
     print(f"Elapsed time in seconds: {elapsed_time}")
-    if elapsed_time> 10:
+    if elapsed_time> SERVER_LIFETIME:
         break
     
     # To talk to the client, we need to use a communication 
